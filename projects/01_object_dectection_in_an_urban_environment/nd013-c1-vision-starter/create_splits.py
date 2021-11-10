5641b3cadb33bf9d38c18426f06e00f8f582630c
Sijoon Lee <shijoonlee@gmail.com>
2021-11-09 20:33:35 -0800

    write create_splits.py

--- projects/01_object_dectection_in_an_urban_environment/nd013-c1-vision-starter/create_splits.py
+++ projects/01_object_dectection_in_an_urban_environment/nd013-c1-vision-starter/create_splits.py
@@ -2,11 +2,47 @@ import argparse
 import glob
 import os
 import random
-
-import numpy as np
+import shutil
 
 from utils import get_module_logger
 
+# useful resources
+# https://stackoverflow.com/questions/46444018/meaning-of-buffer-size-in-dataset-map-dataset-prefetch-and-dataset-shuffle
+# https://stackoverflow.com/questions/51125266/how-do-i-split-tensorflow-datasets/51126863
+# https://stackoverflow.com/questions/54519309/split-tfrecords-file-into-many-tfrecords-files
+# https://www.tensorflow.org/api_docs/python/tf/data/Iterator
+# https://www.tensorflow.org/api_docs/python/tf/experimental/Optional
+
+def split_file_list(all_processed_files, magic_number = 7):
+    # Train set: 60% if magic_number = 7
+    def is_train(x):
+        return x[0] % 10 < magic_number
+
+    # Validation set: 30% if magic_number = 7
+    def is_val(x):
+        return x[0] % 10 > magic_number
+
+    # Test set: always 10%
+    def is_test(x):
+        return x[0] % 10 == magic_number
+
+    recover = lambda x : x[1] # get rid of i from (i, file)
+
+    all_files_enumerated = [ (i, file) for (i, file) in enumerate(all_processed_files) ]
+
+    train_files = list(map(recover, filter(is_train, all_files_enumerated)))
+    val_files = list(map(recover, filter(is_val, all_files_enumerated)))
+    test_files = list(map(recover, filter(is_test, all_files_enumerated)))
+
+    return train_files, val_files, test_files
+
+def copy_files(new_path, files):
+    if not os.path.exists(new_path):
+        os.makedirs(new_path)
+    
+    for file in files:
+        shutil.copy(file, new_path)
+    return
 
 def split(data_dir):
     """
@@ -16,7 +52,23 @@ def split(data_dir):
     args:
         - data_dir [str]: data directory, /mnt/data
     """
-    # TODO: Implement function
+
+    path_processed_files = os.path.join(data_dir, "waymo/processed")
+    all_processed_files = glob.glob(os.path.join(path_processed_files, "*.tfrecord"))
+    random.shuffle(all_processed_files)
+    
+
+    path_train_files = os.path.join(data_dir, "train")
+    path_val_files = os.path.join(data_dir, "val")
+    path_test_files = os.path.join(data_dir, "test")
+
+    train_files, val_files, test_files = split_file_list(all_processed_files)
+
+    copy_files(path_train_files, train_files)
+    copy_files(path_val_files, val_files)
+    copy_files(path_test_files, test_files)
+
+    return
     
 
 if __name__ == "__main__": 
